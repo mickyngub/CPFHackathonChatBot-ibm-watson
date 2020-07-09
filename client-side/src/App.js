b6bf5880b564d547e741ad036b691ca318efb368
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-09 14:48:16 +0700

    Get and handle sessoin_id

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 import { Provider } from "react-redux";
 import store from "./store";
 
 import Chat from "./components/chat/Chat";
+
+import { createSession } from "./actions/watson";
+
+import axios from "axios";
+
+if (!localStorage.session) {
+  axios.defaults.headers.common["session_id"] = localStorage.session;
+} else {
+  delete axios.defaults.headers.common["session_id"];
+}
 const App = () => {
+  useEffect(() => {
+    if (!localStorage.session) {
+      store.dispatch(createSession());
+    }
+  });
   return (
     <Provider store={store}>
       <div>Hey there!</div>;
--- client-side/src/actions/watson.js
+++ client-side/src/actions/watson.js
@@ -1,9 +1,27 @@
-import { INPUT_SUCCESS, INPUT_FAIL } from "./types";
+import {
+  INPUT_SUCCESS,
+  INPUT_FAIL,
+  SESSION_SUCCESS,
+  SESSION_FAIL,
+  MESSAGE_SUCCESS,
+  MESSAGE_FAIL,
+} from "./types";
+
+import axios from "axios";
 
 export const userMessage = (message) => async (dispatch) => {
   try {
     dispatch({ type: INPUT_SUCCESS, payload: message });
   } catch {
     dispatch({ type: INPUT_FAIL });
+  }
+};
+
+export const createSession = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/watson/session");
+    dispatch({ type: SESSION_SUCCESS, payload: res.data });
+  } catch (err) {
+    dispatch({ type: SESSION_FAIL });
   }
 };
